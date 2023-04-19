ed4b614cf3fd5080b9d5f51251681077da0bd6e0
michael-royal <imoleroyal8@gmail.com>
2023-04-19 00:32:14 -0800

    this is my portfolio

---
+++ portflio.js
@@ -0,0 +1,8 @@
+var rip = document.getElementById('rip').addEventListener('click', ()=>{
+  var menu = document.getElementById('menu').classList.toggle('menu')
+})
+
+var view = document.getElementById('view').addEventListener('click', () => {
+  var resume = document.getElementById('resume')
+  resume.classList.toggle('shoe')
+})
